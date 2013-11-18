6285430d8a2412435453eca9662ab0db672eb292
mbatilando <mari.batilando@gmail.com>
2013-11-18 09:37:35 -0800

    Add comments

--- 02_linked_lists/findNthLast/mari.js
+++ 02_linked_lists/findNthLast/mari.js
@@ -4,7 +4,8 @@ var MYAPP = {};
 You can do this problem by having two pointers in that are n elements
 away from each other. Once the further pointer has reached the end of
 the linked list then you know that the other pointer is n elements
-away from the end of the linked list.
+away from the end of the linked list. This algo runs in O(n) time 
+and O(1) space complexity.
 */
 MYAPP.findNthLast = function(head, n) {
 	var farPointer = closePointer = head;
